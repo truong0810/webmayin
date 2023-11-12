d00dbd2b1fae507160121de123951b9889988e96
THAI <xinphatmutikill@gmail.com>
2023-11-12 19:48:39 +0700

    Show images when click

--- pages/layout/printerList.php
+++ pages/layout/printerList.php
@@ -2,6 +2,7 @@
 $sql_category = "SELECT * FROM category ORDER BY id ASC";
 $query_category = mysqli_query($mysqli, $sql_category);
 ?>
+
 <section class="pinter-list">
   <div class="heading-pinter flex items-center justify-between mt-5">
     <div class="flex items-center gap-3 bg-white z-10">
@@ -23,7 +24,48 @@ $query_category = mysqli_query($mysqli, $sql_category);
     </div>
   </div>
 
-  <?php
-  require("categoryPrinterList.php");
-  ?>
+  <div class="product-list mt-10 w-[1495px] overflow-x-auto flex items-center gap-4 flex-wrap mb-10">
+
+    <?php
+    // Check if a category ID is provided in the URL
+    if (isset($_GET['quanly']) && $_GET['quanly'] === 'danhmucsanpham' && isset($_GET['id'])) {
+      // If a category is selected, retrieve products for that category
+      $categoryID = $_GET['id'];
+      $sql_product = "SELECT * FROM product WHERE category_id = $categoryID";
+    } else {
+      // If no category is selected, retrieve random products from any category
+      $sql_product = "SELECT * FROM product WHERE category_id = 3";
+    }
+
+    $query_product = mysqli_query($mysqli, $sql_product);
+    while ($row = mysqli_fetch_array($query_product)) {
+    ?>
+      <div class="product-item border w-[232px] border-graydb rounded-lg">
+        <a href="details.php?id=<?= $row['id'] ?>" class="flex items-center justify-center text-center p-[15px_10px_20px_10px]">
+          <img srcset="admin/modules/products/store/<?= $row['thumbnail'] ?> 2x" class="w-full max-w-[85%] h-full object-cover hover:scale-110 transition-all" />
+        </a>
+        <p class="product-status bg-primary text-center uppercase text-white text-sm font-semibold p-2">
+          Còn hàng
+        </p>
+        <div class="mt-5 px-1 text-center">
+          <a href="details.php?id=<?= $row['id'] ?>" class="hidden-text text-lg font-semibold hover:text-primary transition-all">
+            <?= $row['title'] ?>
+          </a>
+          <p class="text-gray82 font-medium line-through text-lg mt-3">
+            <?= number_format($row['price'], 0, ',', '.') ?> đ
+          </p>
+          <p class="text-secondary font-bold text-2xl"><?= number_format($row['discount'], 0, ',', '.') ?> đ</p>
+          <div class="btn my-5">
+            <a href="#" class="inline-flex items-center justify-center gap-3 bg-primary py-[6px] px-5 rounded-lg text-white uppercase hover:bg-primaryHover transition-all">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              <span>Mua ngay</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    <?php }
+    ?>
+  </div>
 </section>
