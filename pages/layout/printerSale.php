89cfeb435339efda3bb1e91745fc8b7476a9e757
THAI <xinphatmutikill@gmail.com>
2023-11-12 10:53:00 +0700

    Filter by promotional and best-selling products

--- pages/layout/printerSale.php
+++ pages/layout/printerSale.php
@@ -1,124 +1,39 @@
+<?php
+$sql_khuyenmai = "SELECT * FROM product WHERE hot_sale = 1 ORDER BY id ASC";
+$query_khuyenmai = mysqli_query($mysqli, $sql_khuyenmai);
+?>
 <section class="product-filter">
   <div class="mt-10 border-b border-graydb">
-    <ul class="flex items-center justify-center gap-7">
-      <li>
-        <a href="index.php" class="border border-primary px-4 py-3 rounded-full text-base text-primary uppercase font-semibold hover:text-white hover:bg-primary transition-all">Sản phẩm mới</a>
-      </li>
-      <li>
-        <a href="index.php?quanly=sanphambanchay&id=1" class="border border-primary px-4 py-3 rounded-full text-base text-primary uppercase font-semibold hover:text-white hover:bg-primary transition-all">Bán chạy</a>
-      </li>
-      <li>
-        <a href="index.php?quanly=sanphamkhuyenmai&id=1" class="product-filter-active  border border-primary px-4 py-3 rounded-full text-base text-primary uppercase font-semibold hover:text-white hover:bg-primary transition-all">Khuyến mãi sốc</a>
-      </li>
-      <li>
-        <a href="#" class="border border-primary px-4 py-3 rounded-full text-base text-primary uppercase font-semibold hover:text-white hover:bg-primary transition-all">Xem gần đây</a>
-      </li>
-    </ul>
+    <?php require("navigationPrinterFilter.php") ?>
 
     <div class="product-list mt-10 w-[1495px] overflow-x-auto flex items-center gap-4 flex-wrap mb-10">
-      <div class="product-item border w-[232px] border-graydb rounded-lg">
-        <a class="flex items-center justify-center text-center p-[15px_10px_20px_10px]">
-          <img srcset="./pages/image/product/pr1.jpg 2x" class="w-full max-w-[85%] h-full object-cover hover:scale-110 transition-all" />
-        </a>
-        <p class="product-status bg-primary text-center uppercase text-white text-sm font-semibold p-2">
-          Còn hàng
-        </p>
-        <div class="mt-5 px-1 text-center">
-          <a class="hidden-text text-lg font-semibold hover:text-primary transition-all">
-            Máy scan không dây HP Enterprise Flow N7000 snw1 (6FW10A)
+      <?php while ($row = mysqli_fetch_array($query_khuyenmai)) { ?>
+        <div class="product-item border w-[232px] border-graydb rounded-lg">
+          <a class="flex items-center justify-center text-center p-[15px_10px_20px_10px]">
+            <img srcset="admin/modules/products/store/<?= $row['thumbnail'] ?> 2x" class="w-full max-w-[85%] h-full object-cover hover:scale-110 transition-all" />
           </a>
-          <p class="text-gray82 font-medium line-through text-lg mt-3">
-            25.850.000 đ
+          <p class="product-status bg-primary text-center uppercase text-white text-sm font-semibold p-2">
+            Còn hàng
           </p>
-          <p class="text-secondary font-bold text-2xl">25.850.000 đ</p>
-          <div class="btn my-5">
-            <a href="/" class="inline-flex items-center justify-center gap-3 bg-primary py-[6px] px-5 rounded-lg text-white uppercase hover:bg-primaryHover transition-all">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-              <span>Mua ngay</span>
+          <div class="mt-5 px-1 text-center">
+            <a class="hidden-text text-lg font-semibold hover:text-primary transition-all">
+              <?= $row['title'] ?>
             </a>
+            <p class="text-gray82 font-medium line-through text-lg mt-3">
+              <?= number_format($row['price'], 0, ',', '.') ?> đ
+            </p>
+            <p class="text-secondary font-bold text-2xl"><?= number_format($row['discount'], 0, ',', '.') ?> đ</p>
+            <div class="btn my-5">
+              <a href="#" class="inline-flex items-center justify-center gap-3 bg-primary py-[6px] px-5 rounded-lg text-white uppercase hover:bg-primaryHover transition-all">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span>Mua ngay</span>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div class="product-item border w-[232px] border-graydb rounded-lg">
-        <a class="flex items-center justify-center text-center p-[15px_10px_20px_10px]">
-          <img srcset="./pages/image/product/pr1.jpg 2x" class="w-full max-w-[85%] h-full object-cover hover:scale-110 transition-all" />
-        </a>
-        <p class="product-status bg-primary text-center uppercase text-white text-sm font-semibold p-2">
-          Còn hàng
-        </p>
-        <div class="mt-5 px-1 text-center">
-          <a class="hidden-text text-lg font-semibold hover:text-primary transition-all">
-            Máy scan không dây HP Enterprise Flow N7000 snw1 (6FW10A)
-          </a>
-          <p class="text-gray82 font-medium line-through text-lg mt-3">
-            25.850.000 đ
-          </p>
-          <p class="text-secondary font-bold text-2xl">25.850.000 đ</p>
-          <div class="btn my-5">
-            <a href="/" class="inline-flex items-center justify-center gap-3 bg-primary py-[6px] px-5 rounded-lg text-white uppercase hover:bg-primaryHover transition-all">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-              <span>Mua ngay</span>
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div class="product-item border w-[232px] border-graydb rounded-lg">
-        <a class="flex items-center justify-center text-center p-[15px_10px_20px_10px]">
-          <img srcset="./pages/image/product/pr1.jpg 2x" class="w-full max-w-[85%] h-full object-cover hover:scale-110 transition-all" />
-        </a>
-        <p class="product-status bg-primary text-center uppercase text-white text-sm font-semibold p-2">
-          Còn hàng
-        </p>
-        <div class="mt-5 px-1 text-center">
-          <a class="hidden-text text-lg font-semibold hover:text-primary transition-all">
-            Máy scan không dây HP Enterprise Flow N7000 snw1 (6FW10A)
-          </a>
-          <p class="text-gray82 font-medium line-through text-lg mt-3">
-            25.850.000 đ
-          </p>
-          <p class="text-secondary font-bold text-2xl">25.850.000 đ</p>
-          <div class="btn my-5">
-            <a href="/" class="inline-flex items-center justify-center gap-3 bg-primary py-[6px] px-5 rounded-lg text-white uppercase hover:bg-primaryHover transition-all">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-              <span>Mua ngay</span>
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div class="product-item border w-[232px] border-graydb rounded-lg">
-        <a class="flex items-center justify-center text-center p-[15px_10px_20px_10px]">
-          <img srcset="./pages/image/product/pr1.jpg 2x" class="w-full max-w-[85%] h-full object-cover hover:scale-110 transition-all" />
-        </a>
-        <p class="product-status bg-primary text-center uppercase text-white text-sm font-semibold p-2">
-          Còn hàng
-        </p>
-        <div class="mt-5 px-1 text-center">
-          <a class="hidden-text text-lg font-semibold hover:text-primary transition-all">
-            Máy scan không dây HP Enterprise Flow N7000 snw1 (6FW10A)
-          </a>
-          <p class="text-gray82 font-medium line-through text-lg mt-3">
-            25.850.000 đ
-          </p>
-          <p class="text-secondary font-bold text-2xl">25.850.000 đ</p>
-          <div class="btn my-5">
-            <a href="/" class="inline-flex items-center justify-center gap-3 bg-primary py-[6px] px-5 rounded-lg text-white uppercase hover:bg-primaryHover transition-all">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-              <span>Mua ngay</span>
-            </a>
-          </div>
-        </div>
-      </div>
+      <?php } ?>
     </div>
   </div>
 </section>
